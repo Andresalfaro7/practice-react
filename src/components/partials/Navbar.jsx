f8871f5c6d820ea18e729d51060908f46036f508
Andresalfaro7 <andresalfaroparada@gmail.com>
2024-04-10 14:31:35 -0600

    First commit

---
+++ src/components/partials/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () =>{
+    return(
+        <nav>
+            <h2>Este es mi navbar</h2>
+            <a href="/nosotros">Ir a nosotros</a>
+        </nav>
+        
+    );
+}
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () =>{
+    return(
+        <h1>Esta es mi pagina de Inicio</h1>
+    );
+}
+
+export default Home;
