de462ccc505842b126b44ab220a5ff4576ec9444
Andresalfaro7 <andresalfaroparada@gmail.com>
2024-04-15 09:28:04 -0600

    View Form

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () =>{
     return(
         <nav>
-            <h2>Este es mi navbar</h2>
+            <h2 className="text-red-500">Este es mi navbar</h2>
             <a href="/nosotros">Ir a nosotros</a>
         </nav>
         
