725a9ca5573e17f79843a6c04a3b1dcda386db80
Andresalfaro7 <andresalfaroparada@gmail.com>
2024-04-15 11:08:54 -0600

    Add modal AddProduct

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,81 +1,78 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ModalAddProduct from '../components/ModalAddProduct';
 
 const Home = () => {
 
+    const [data, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
     const apiUrl = 'http://localhost/products-api/api.php';
-    let config ={
-        headers:{
+    let config = {
+        headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         }
     }
 
-    const getAllProducts = async() =>{
+    const getAllProducts = async () => {
         const res = await axios.get(`${apiUrl}/productos`, config);
         console.log(res);
+        setData(res.data);
     }
 
     useEffect(() => {
         getAllProducts();
     }, []);
     
+    if (10 > 2) {
+        console.log("Si es mayor")
+    } else {
+        console.log("NO es mayor, puede que se ha igual");
+    }
+
+    10 > 2 ? console.log("Si es mayor") : console.log("NO es mayor, puede que se ha igual"); 
+
 
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Pricing</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p>
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+                        Productos
+                        <button 
+                            className="text-base flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
+                            onClick={() =>{setShowModal(true)}}
+                        >
+                            Agregar
+                        </button>
+                    </h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Productos en existencia</p>
                 </div>
-                <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+                <div className="w-full mx-auto overflow-auto">
                     <table className="table-auto w-full text-left whitespace-no-wrap">
                         <thead>
                             <tr>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Plan</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Speed</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Storage</th>
-                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price</th>
-                                <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Nombre</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Descripción</th>
+                                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Precio</th>
+                                <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="px-4 py-3">Start</td>
-                                <td className="px-4 py-3">5 Mb/s</td>
-                                <td className="px-4 py-3">15 GB</td>
-                                <td className="px-4 py-3 text-lg text-gray-900">Free</td>
-                                <td className="w-10 text-center">
-                                    <input name="plan" type="radio"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">Pro</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">25 Mb/s</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">25 GB</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$24</td>
-                                <td className="border-t-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">Business</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">36 Mb/s</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3">40 GB</td>
-                                <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$50</td>
-                                <td className="border-t-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">Exclusive</td>
-                                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">48 Mb/s</td>
-                                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
-                                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$72</td>
-                                <td className="border-t-2 border-b-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio"/>
-                                </td>
-                            </tr>
+                            {data.map((item, key) => (
+                                <tr key={key}>
+                                    <td className="px-4 py-3">{item.id}</td>
+                                    <td className="px-4 py-3">{item.nombre}</td>
+                                    <td className="px-4 py-3">{item.descripcion}</td>
+                                    <td className="px-4 py-3 text-lg text-gray-900">{item.precio}</td>
+                                    <td className="w-10 text-center flex">
+                                        <button className="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Editar</button>
+                                        <button className="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Eliminar</button>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
@@ -88,6 +85,7 @@ const Home = () => {
                     <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
                 </div>
             </div>
+            {showModal && <ModalAddProduct setShowModal={setShowModal}/>}
         </section>
     );
 }
